ca9913b5ea95bc4bc5915a333be8f830844d095e
Davidgraja <juliograjalesparedes127@gmail.com>
2022-04-15 12:29:26 +0200

    encapsulamiento de los calculos en funciones expresivas , teniendo un codigo mas resumido y ordenado

--- figuras.js
+++ figuras.js
@@ -1,60 +1,25 @@
 //codigo cuadrado
-console.group("cuadrados");
 
+const perimetroCuadrado= (lado) =>   lado * 4 + " cm" ;
 
-function perimetroCuadrado(lado) {
-  return  lado * 4 + " cm" ;
-}
-
-
-function  areaCuadrado(lado) {
-  return  lado * lado + " cm^2";
-}
-
-
-console.groupEnd();
-
+const  areaCuadrado= (lado) =>   lado * lado + " cm^2";
 
 //codigo triangulo
 
-console.group("triangulo")
+const perimetroTriangulo= (lado1 , lado2 , base ) =>  lado1 +lado2 + base  + " cm";
 
-
-function perimetroTriangulo(lado1 , lado2 , base ){
-  return lado1 +lado2 + base  + " cm";
-}
-
-
-function  areaTriangulo(base, altura){
-  return (base *  altura ) / 2 + " cm^2";
-}
-
-console.groupEnd();
-
+const  areaTriangulo=(base, altura)=>(base *  altura ) / 2 + " cm^2";
 
 //codigo  circuferencia
-console.group("circulo")
-
 
 //Diametro
-
-function diametroCirculo(radio){
-  return radio * 2 ;
-}
+const diametroCirculo= (radio) =>  radio * 2 ;
 
 //PI
 const PI = Math.PI;
 
 //perimetro
-function perimetroCirculo(radio){
-   let diametro = diametroCirculo(radio);
-   return  diametro * PI;
- }
+const  perimetroCirculo=(radio) =>  diametroCirculo(radio) * PI;
 
 //area
-function areaCirculo(radio){
-    return ( radio * radio ) * PI + " cm^2";
-}
-
-
-console.groupEnd()
+const areaCirculo= (radio) => ( radio * radio ) * PI + " cm^2";
