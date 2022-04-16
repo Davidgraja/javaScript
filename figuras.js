1d9032b1feddab46caa28bbf14e21533b75274b9
Davidgraja <juliograjalesparedes127@gmail.com>
2022-04-16 12:43:19 +0200

    implementacion de javaScript  al proyecto

--- figuras.js
+++ figuras.js
@@ -19,32 +19,91 @@ const diametroCirculo= (radio) =>  radio * 2 ;
 const PI = Math.PI;
 
 //perimetro
-const  perimetroCirculo=(radio) =>  diametroCirculo(radio) * PI;
+const  perimetroCirculo=(radio) =>  diametroCirculo(radio) * PI ;
 
 //area
-const areaCirculo= (radio) => ( radio * radio ) * PI + " cm^2";
+const areaCirculo= (radio) => ( radio * radio ) * PI ;
 
 // interaccion con el html
 
+// obtenccion   las etiquetas padres , del formulario y de la entrega de informacion 
 const inputs= document.getElementById('inputs')
-console.log(inputs)
+const resultado= document.getElementById('entrega-resultados')
+
+//creaccion de etiquetas para la seccion Resultados 
+/** */
+var circuloMensaje="";
+
+const perimetro = document.createElement('p');
+perimetro.textContent='perimetro'; 
+
+const diametro= document.createElement('p')
+diametro.textContent="Diametro";
+
+const area= document.createElement('p');
+area.textContent="Area";
+
+const detallePerimetro = document.createElement('div');
+const detalleDiametro= document.createElement('div');
+const detalleArea=document.createElement('div');
 
 
-const buttomFormulario = document.getElementById('buttom') 
+
+// evento circulo
+const elementoHtml= document.createElement('label');
+elementoHtml.textContent='Radio'; 
+const inputRadio= document.createElement('input'); 
+
 const circulo = document.getElementById('circulo') ;
+circulo.addEventListener('click', circuloEvento);
+ function circuloEvento(){
+    inputs.appendChild(elementoHtml) ;  
+    elementoHtml.appendChild(inputRadio)
 
-circulo.addEventListener('click', circuloFuncion)
 
-
-function circuloFuncion(){
-    //inputs.innerHTML="<label > lado <input id='lado' type='number'/ ></label>"
-    
-    
+    resultado.appendChild(diametro);
+    resultado.appendChild(detalleDiametro);
+    resultado.appendChild(perimetro);
+    resultado.appendChild(detallePerimetro);
+    resultado.appendChild(area);
+    resultado.appendChild(detalleArea);
+    circuloMensaje="circulo";
 }
 
+//evento trinagulo
+const lado1 = document.createElement('label');
+lado1.textContent="Lado 1";
+const inputLado1 = document.createElement("input");
 
-const ladoCirculo= document.getElementById('lado')
-buttomFormulario.addEventListener('click', buttonFuncion)
+const lado2 =document.createElement('label');
+lado2.textContent="Lado 2 ";
+const inputLado2= document.createElement('input');
+
+const triangulo= document.getElementById('triangulo');
+triangulo.addEventListener('click',trianguloEvento);
+function trianguloEvento(){
+    inputs.appendChild(lado1);
+    lado1.appendChild(inputLado1);
+    inputs.appendChild(lado2);
+    lado2.appendChild(inputLado2);
+}
+
+// evento boton calcular 
+const buttomCalcular = document.getElementById('buttom');
+buttomCalcular.addEventListener('click', buttonFuncion)
 function buttonFuncion(){
-    console.log(inputs)
+
+    inputs.onsubmit=(e)=>{
+        e.preventDefault();
+
+        if(circuloMensaje == "circulo"){
+            const valorRadio = inputRadio.value;
+            inputRadio.value = "";
+            detallePerimetro.innerText=  perimetroCirculo(valorRadio ) + " cm";
+            detalleDiametro.innerText=diametroCirculo(valorRadio ) + " cm";
+            detalleArea.innerText= areaCirculo(valorRadio) +  " cm^2"
+
+        }
+        
+    }
 }
