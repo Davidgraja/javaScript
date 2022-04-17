51001da647249747dbcea66a32d7809dd765b00f
Davidgraja <juliograjalesparedes127@gmail.com>
2022-04-17 20:05:13 +0200

    integracion de javaScript a  las secciones de inpust y resulatos , aun falta mas codigo que aplicar

--- figuras.js
+++ figuras.js
@@ -6,9 +6,9 @@ const  areaCuadrado= (lado) =>   lado * lado + " cm^2";
 
 //codigo triangulo
 
-const perimetroTriangulo= (lado1 , lado2 , base ) =>  lado1 +lado2 + base  + " cm";
+const perimetroTriangulo= (lado1 , lado2 , base ) =>  lado1 +lado2 + base;
 
-const  areaTriangulo=(base, altura)=>(base *  altura ) / 2 + " cm^2";
+const  areaTriangulo=(base, altura)=>(base *  altura ) / 2 ;
 
 //codigo  circuferencia
 
@@ -32,7 +32,7 @@ const resultado= document.getElementById('entrega-resultados')
 
 //creaccion de etiquetas para la seccion Resultados 
 /** */
-var circuloMensaje="";
+var eventoMensaje="";
 
 const perimetro = document.createElement('p');
 perimetro.textContent='perimetro'; 
@@ -67,17 +67,26 @@ circulo.addEventListener('click', circuloEvento);
     resultado.appendChild(detallePerimetro);
     resultado.appendChild(area);
     resultado.appendChild(detalleArea);
-    circuloMensaje="circulo";
+    eventoMensaje="circulo";
 }
 
 //evento trinagulo
-const lado1 = document.createElement('label');
+var lado1 = document.createElement('label');
 lado1.textContent="Lado 1";
 const inputLado1 = document.createElement("input");
 
-const lado2 =document.createElement('label');
-lado2.textContent="Lado 2 ";
+var lado2 =document.createElement('label');
+lado2.textContent="Lado 2";
 const inputLado2= document.createElement('input');
+
+const altura = document.createElement('label');
+altura.textContent = 'Altura'
+const inputAltura = document.createElement('input');
+
+const base = document.createElement('label');
+base.textContent="Base";
+const inputBase = document.createElement("input");
+
 
 const triangulo= document.getElementById('triangulo');
 triangulo.addEventListener('click',trianguloEvento);
@@ -86,7 +95,37 @@ function trianguloEvento(){
     lado1.appendChild(inputLado1);
     inputs.appendChild(lado2);
     lado2.appendChild(inputLado2);
+    inputs.appendChild(base);
+    base.appendChild(inputBase);
+    inputs.appendChild(altura);
+    altura.appendChild(inputAltura);
+
+    resultado.appendChild(perimetro);
+    resultado.appendChild(detallePerimetro);
+    resultado.appendChild(area);
+    resultado.appendChild(detalleArea);
+
+    eventoMensaje="triangulo";
 }
+
+//evento cuadrado
+const cuadrado = document.getElementById('cuadrado');
+cuadrado.addEventListener('click',cuadradoEvento);
+function cuadradoEvento(){
+    lado1.textContent="Lado"
+    inputs.appendChild(lado1);
+    lado1.appendChild(inputLado1);
+
+    resultado.appendChild(perimetro);
+    resultado.appendChild(detallePerimetro);
+    resultado.appendChild(area);
+    resultado.appendChild(detalleArea);
+
+    eventoMensaje="cuadrado";
+}
+
+
+
 
 // evento boton calcular 
 const buttomCalcular = document.getElementById('buttom');
@@ -96,12 +135,36 @@ function buttonFuncion(){
     inputs.onsubmit=(e)=>{
         e.preventDefault();
 
-        if(circuloMensaje == "circulo"){
+        if(eventoMensaje == "circulo"){
             const valorRadio = inputRadio.value;
             inputRadio.value = "";
-            detallePerimetro.innerText=  perimetroCirculo(valorRadio ) + " cm";
-            detalleDiametro.innerText=diametroCirculo(valorRadio ) + " cm";
-            detalleArea.innerText= areaCirculo(valorRadio) +  " cm^2"
+            detallePerimetro.innerText=  perimetroCirculo(valorRadio ) ;
+            detalleDiametro.innerText=diametroCirculo(valorRadio ) ;
+            detalleArea.innerText= areaCirculo(valorRadio); 
+
+        }
+
+        else if(eventoMensaje == "triangulo" ){
+            const  valorLado1= inputLado1.value;
+            const valorLado2= inputLado2.value;
+            const valorBase = inputBase.value;
+            const valorAltura= inputAltura.value;
+            inputLado1.value="";
+            inputLado2.value= "";
+            inputBase.value= "";
+            inputAltura.value="";
+            
+            detallePerimetro.innerText = perimetroTriangulo(valorLado1,valorLado2, valorBase);
+            detalleArea.innerText= areaTriangulo(valorBase, valorAltura);
+            
+
+        }
+
+        else if(eventoMensaje=="cuadrado"){
+            const valorlado= inputLado1.value;
+            inputLado1.value="";
+            detallePerimetro.innerText = perimetroCuadrado(valorlado);
+            detalleArea.innerText = areaCuadrado(valorlado);
 
         }
         
